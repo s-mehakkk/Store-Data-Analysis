f8eb12fa17e4a3f17b5ec106dea39d8f5409881a
s-mehakkk <75841992+s-mehakkk@users.noreply.github.com>
2023-07-25 20:07:32 +0530

    Adding create table and inserting value files

---
+++ create_table.sql
@@ -0,0 +1,115 @@
+USE storeData;
+
+CREATE TABLE customers (
+  customerNumber integer NOT NULL,
+  customerName varchar(50) NOT NULL,
+  contactLastName varchar(50) NOT NULL,
+  contactFirstName varchar(50) NOT NULL,
+  phone varchar(50) NOT NULL,
+  addressLine1 varchar(50) NOT NULL,
+  addressLine2 varchar(50) DEFAULT NULL,
+  city varchar(50) NOT NULL,
+  state varchar(50) DEFAULT NULL,
+  postalCode varchar(15) DEFAULT NULL,
+  country varchar(50) NOT NULL,
+  salesRepEmployeeNumber integer DEFAULT NULL,
+  creditLimit numeric(10,2) DEFAULT NULL,
+  PRIMARY KEY (customerNumber)
+);
+
+CREATE TABLE employees (
+  employeeNumber integer NOT NULL,
+  lastName varchar(50) NOT NULL,
+  firstName varchar(50) NOT NULL,
+  extension varchar(10) NOT NULL,
+  email varchar(100) NOT NULL,
+  officeCode varchar(10) NOT NULL,
+  reportsTo integer DEFAULT NULL,
+  jobTitle varchar(50) NOT NULL,
+  PRIMARY KEY (employeeNumber)
+);
+
+CREATE TABLE offices (
+  officeCode varchar(10) NOT NULL,
+  city varchar(50) NOT NULL,
+  phone varchar(50) NOT NULL,
+  addressLine1 varchar(50) NOT NULL,
+  addressLine2 varchar(50) DEFAULT NULL,
+  state varchar(50) DEFAULT NULL,
+  country varchar(50) NOT NULL,
+  postalCode varchar(15) NOT NULL,
+  territory varchar(10) NOT NULL,
+  PRIMARY KEY (officeCode)
+);
+
+CREATE TABLE orderdetails (
+  orderNumber integer NOT NULL,
+  productCode varchar(15) NOT NULL,
+  quantityOrdered integer NOT NULL,
+  priceEach numeric(10,2) NOT NULL,
+  orderLineNumber smallint(6) NOT NULL,
+  PRIMARY KEY (orderNumber,productCode)
+);
+
+CREATE TABLE orders (
+  orderNumber integer NOT NULL,
+  orderDate date NOT NULL,
+  requiredDate date NOT NULL,
+  shippedDate date DEFAULT NULL,
+  status varchar(15) NOT NULL,
+  comments text,
+  customerNumber integer NOT NULL,
+  PRIMARY KEY (orderNumber)
+);
+
+CREATE TABLE payments (
+  customerNumber integer NOT NULL,
+  checkNumber varchar(50) NOT NULL,
+  paymentDate date NOT NULL,
+  amount numeric(10,2) NOT NULL,
+  PRIMARY KEY (customerNumber,checkNumber)
+);
+
+CREATE TABLE productlines (
+  productLine varchar(50) NOT NULL,
+  textDescription varchar(4000) DEFAULT NULL,
+  htmlDescription mediumtext,
+  image mediumblob,
+  PRIMARY KEY (productLine)
+);
+
+CREATE TABLE products (
+  productCode varchar(15) NOT NULL,
+  productName varchar(70) NOT NULL,
+  productLine varchar(50) NOT NULL,
+  productScale varchar(10) NOT NULL,
+  productVendor varchar(50) NOT NULL,
+  productDescription text NOT NULL,
+  quantityInStock smallint(6) NOT NULL,
+  buyPrice numeric(10,2) NOT NULL,
+  MSRP numeric(10,2) NOT NULL,
+  PRIMARY KEY (productCode)
+);
+
+-- adding foreign keys
+
+ALTER TABLE customers
+ADD FOREIGN KEY (salesRepEmployeeNumber) REFERENCES employees (employeeNumber);
+
+ALTER TABLE employees
+ADD FOREIGN KEY (reportsTo) REFERENCES employees (employeeNumber),
+ADD  FOREIGN KEY (officeCode) REFERENCES offices (officeCode);
+
+ALTER TABLE orderdetails
+ADD FOREIGN KEY (orderNumber) REFERENCES orders (orderNumber),
+ADD  FOREIGN KEY (productCode) REFERENCES products (productCode);
+
+ALTER TABLE orders
+ADD FOREIGN KEY (customerNumber) REFERENCES customers (customerNumber);
+
+ALTER TABLE payments
+ADD FOREIGN KEY (customerNumber) REFERENCES customers (customerNumber);
+
+ALTER TABLE products
+ADD FOREIGN KEY (productLine) REFERENCES productlines (productLine);
+
